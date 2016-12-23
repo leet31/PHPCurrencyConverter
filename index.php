46a3a6da0eb02af5c44759618312610fd8412179
Tiffany <Tiffany@DESKTOP-2V73ATE>
2016-12-23 18:44:40 -0500

    replaced php code at top

--- index.php
+++ index.php
@@ -1,4 +1,60 @@
 <!DOCTYPE html>
+   <?php
+     if(isset($_GET['sbmt']))
+     {
+         
+        $cc_input = $_GET['input'];
+        $cc_dropdown1 =$_GET['dropdown1'];
+        $cc_dropdown2 = $_GET['dropdown2'];
+       
+        if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'usd')
+        {
+            $output = $cc_input*1;
+              
+        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'euro'){
+            $output = $cc_input*0.96;
+             
+        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'jpy'){
+            $output = $cc_input*117.92;
+                
+        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'germ'){
+            $output = $cc_input*1.876;
+           
+        }else if($cc_dropdown1 = 'euro' AND $cc_dropdown2 == 'usd'){
+            $output = $cc_input*1.04;
+            
+        }else if($cc_dropdown1 = 'euro' AND $cc_dropdown2 == 'euro'){
+            $output = $cc_input*1;
+            
+        }else if($cc_dropdown1 == 'euro' AND $cc_dropdown2 == 'jpy'){
+            $output = $cc_input*122.83;
+            
+        }else if($cc_dropdown1 = 'euro' AND $cc_dropdown2 == 'germ'){
+            $output = $cc_input*1.954;
+         
+        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'usd'){
+            $output = $cc_input*0.0085;
+            
+        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'euro'){
+            $output = $cc_input*0.0082;
+            
+        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'jpy'){
+            $output = $cc_input*1;
+           
+        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'germ'){
+            $output = $cc_input*0.015;
+            
+        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'usd'){
+            $output = $cc_input*1.955;
+            
+        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'jpy'){
+            $output = $cc_input*62.77;
+            
+        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'euro'){
+            $output = $cc_input*0.51;
+            }
+     }
+     ?>
 <html>
  <head>
 <meta charset="utf-8">
@@ -25,8 +81,9 @@
     <option value="jpy">Japanese Yen</option>
     <option value="germ">German Mark</option>
   </select>
-     <input type="text" name ="output" value ="<?php if (isset($output))echo $output ?>">
+    
     <input type="submit" name="sbmt" value="Convert=>"/>
+     <input type="text" name ="output" value ="<?php if (isset($output))echo $output ?>">
    <select name="dropdown2">
     <option value="usd">US Dollar</option>
     <option value="euro">Euro</option>
@@ -35,82 +92,5 @@
    </select>
      
 </form>
-        <?php
-     if(isset($_GET['sbmt']))
-     {
-         
-        $cc_input = $_GET['input'];
-        $cc_dropdown1 =$_GET['dropdown1'];
-        $cc_dropdown2 = $_GET['dropdown2'];
-        $output = $_GET['output'];
-        
-        if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'usd')
-        {
-            $output = $cc_input*1;
-              
-        
-        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'euro'){
-            $output = $cc_input*0.96;
-            echo $output; 
-            
-        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'jpy'){
-            $output = $cc_input*117.92;
-            echo $output;  
-            
-        }else if($cc_dropdown1 == 'usd' AND $cc_dropdown2 == 'germ'){
-            $output = $cc_input*1.876;
-            echo $output; 
-            
-        }else if($cc_dropdown1 == 'euro' AND $cc_dropdown2 == 'usd'){
-            $output = $cc_input*0;
-            echo $output;
-            
-            
-        }else if($cc_dropdown1 = 'euro' AND $cc_dropdown2 == 'euro'){
-            $output = $cc_input*1;
-            echo $output;
-            
-        }else if($cc_dropdown1 == 'euro' AND $cc_dropdown2 == 'jpy'){
-            $output = $cc_input*122.83;
-            echo $output;
-        
-        }else if($cc_dropdown1 = 'euro' AND $cc_dropdown2 == 'germ'){
-            $output = $cc_input*1.954;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'usd'){
-            $output = $cc_input*0;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'euro'){
-            $output = $cc_input*0;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'jpy'){
-            $output = $cc_input*1;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'jpy' AND $cc_dropdown2 == 'germ'){
-            $output = $cc_input*62.857;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'usd'){
-            $output = $cc_input*0;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'euro'){
-            $output = $cc_input*0;
-            echo $output;
-            
-        }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'jpy'){
-            $output = $cc_input*0;
-            echo $output;
-            
-            }else if($cc_dropdown1 = 'germ' AND $cc_dropdown2 == 'germ'){
-            $output = $cc_input*1;
-            echo $output;
-            }
-     }
-?>
 </body>
 </html>
